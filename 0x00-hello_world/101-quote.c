2521902799c701e105e2d94edde57fe971641a4c
aymane <minescopola@gmail.com>
2023-07-13 20:15:48 +0000

    okay

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <unistd.h>
-int main()
-  {
-  char x[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-  write(1,&x,59); // Feel free to check the return value.
-  return 0;
-  }
+/**
+*main - function that prints a specific message
+*@void: void
+(*
+*this function prints a message using the write function)?
+*Return: returns zero
+*/
+int main(void)
+{
+	char x[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, &x, 59);
+
+	return (1);
+}
