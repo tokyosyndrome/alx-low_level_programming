e0efd10a0285ff14ee526566b7c616bef025fcf3
aymane <minescopola@gmail.com>
2023-07-13 19:36:11 +0000

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - function that prints a specific message
+*@void: void
+(*
+*this function prints a message and returns zero)?
+*Return: returns zero
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
