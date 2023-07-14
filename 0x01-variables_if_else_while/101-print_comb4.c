9261a279ea3f4cee1840dc0dbadadc908be53499
aymane <minescopola@gmail.com>
2023-07-14 22:52:40 +0000

    Okay

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - function that prints the alphabet using putchat
+*@void: void
+(*
+*this function prints a message and returns zero)?
+*Return: returns zero
+*/
+int main(void)
+{
+	int i;
+
+	int j;
+
+	int k;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = i + 1; j < 9; j++)
+		{
+			for (k = i + 2; k < 10; k++)
+			{
+				putchar(48 + i);
+				putchar(48 + j);
+				putchar(48 + k);
+				if (!(i == 7 && j == 8 && k == 9))
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
