5ac9d539b35fafd59eb7bb4c55191f3e9f7ab370
aymane <minescopola@gmail.com>
2023-07-14 22:38:11 +0000

    ok

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,11 +16,11 @@ int main(void)
 
 	for (i = 0; i < 9; i++)
 	{
-		for (j = i+1; j < 10; j++)
+		for (j = i + 1; j < 10; j++)
 		{
 				putchar(48 + i);
 				putchar(48 + j);
-				if( !(i == 8 && j == 9) )
+				if (!(i == 8 && j == 9))
 				{
 					putchar(44);
 					putchar(32);
