ad6e41945e9a25b758521c019d326e2c9d756495
aymane <minescopola@gmail.com>
2023-07-19 19:11:29 +0000

    header

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
 *main - function that prints a specific message
 *@void: void
@@ -8,7 +8,16 @@
 */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
+
 	return (0);
 }
 
