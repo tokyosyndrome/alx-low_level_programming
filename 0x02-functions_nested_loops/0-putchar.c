cec65a94e3901ba590f1f4f468772a081d791589
aymane <minescopola@gmail.com>
2023-07-19 17:31:21 +0000

    ok\

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - function that prints a specific message
+*@void: void
+(*
+*this function prints a message and returns zero)?
+*Return: returns zero
+*/
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
+
