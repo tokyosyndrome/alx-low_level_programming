4a6a2b03e2dfea3d6a40a72e7fbe18afb5679184
aymane <minescopola@gmail.com>
2023-07-19 20:54:17 +0000

    comit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include <stdio.h>;
+#include "main.h";
+/**
+*print_sign - function that returns a numbe's sign
+*@c: int variable
+(*
+*this function prints a message and returns zero)?
+*Return: returns zero or one
+*/
+int print_sign(int c)
+{	
+	if (c > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (c < 0)
+	{
+		_putchar('-');
+		return (-1)
+	}
+	if (c == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+}
