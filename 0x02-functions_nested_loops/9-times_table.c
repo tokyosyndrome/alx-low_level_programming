aba6be726a3123be9e25344b919d75a790b1ae51
aymane <minescopola@gmail.com>
2023-07-19 22:09:58 +0000

    yeah

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -33,8 +33,8 @@ void times_table(void)
 
 			if ((num / 10) != 0)
 			{
-				_putchar(48 + (num / 10);
-				_putchar(48 + (num % 10);
+				_putchar(48 + (num / 10));
+				_putchar(48 + (num % 10));
 			}
 			else
 			{
