adf05f485664f4ecb86abb275b52b5be5a896574
aymane <minescopola@gmail.com>
2023-07-19 21:47:11 +0000

    yeah

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -17,12 +17,12 @@ void jack_bauer(void)
 	{
 		for (j = 0; j<60; j++)
 		{
-			_putchar(48 + (i/10));
-			_putchar(48 + (i%10));
+			_putchar(48 + (i / 10));
+			_putchar(48 + (i % 10));
 			_putchar(':');
-			_putchar(48 + (j/10));
-			_putchar(48 + (j%10));
+			_putchar(48 + (j / 10));
+			_putchar(48 + (j % 10));
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
