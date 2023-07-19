2acd9d5b8257e1daefcb62c88dc242fcdf1cba79
aymane <minescopola@gmail.com>
2023-07-19 20:18:42 +0000

    10alpha

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*print_alphabet - function that prints a specific message
+*@void: void
+(*
+*this function prints a message and returns zero)?
+*Return: returns zero
+*/
+
+void print_alphabet_x10(void)
+{
+	char i;
+	int j;
+	for (j = 0; j<10; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+
+
+	}
+}
