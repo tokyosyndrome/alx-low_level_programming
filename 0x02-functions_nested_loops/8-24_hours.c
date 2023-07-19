5a36cd4950428cfcd776e9216449b10774724d83
aymane <minescopola@gmail.com>
2023-07-19 21:45:07 +0000

    jack

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*jack_bauer - function that returns a numbe's sign
+*@void: void
+(*
+*this function prints a message and returns zero)?
+*Return: returns zero or one
+*/
+void jack_bauer(void)
+{
+	int i;
+
+	int j;
+
+	for (i = 0; i<24; i++)
+	{
+		for (j = 0; j<60; j++)
+		{
+			_putchar(48 + (i/10));
+			_putchar(48 + (i%10));
+			_putchar(':');
+			_putchar(48 + (j/10));
+			_putchar(48 + (j%10));
+		}
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -16,4 +16,6 @@ int print_sign(int n);
 int _abs(int c);
 
 int print_last_digit(int);
+
+void jack_bauer(void);
 #endif
