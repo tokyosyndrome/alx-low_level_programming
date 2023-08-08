a7df04361d2c309ef53d25c87682d00f07bb5e86
aymane <minescopola@gmail.com>
2023-08-08 23:29:39 +0000

    ex 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,16 +17,16 @@ char *str_concat(char *s1, char *s2)
 	int i;
 	int j;
 
-	if (!s1 && !s2)
+	if (!s1 || !s2)
 	{
 		return (NULL);
 	}
-	if (s1)
+	if (s1 != NULL)
 	{
 		for (len1 = 0; s1[len1]; len1++)
 			;
 	}
-	if (s2)
+	if (s2 != NULL)
 	{
 		for (len2 = 0; s2[len2]; len2++)
 			;
