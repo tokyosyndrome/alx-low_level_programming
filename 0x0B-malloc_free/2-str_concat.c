3ad9cbf7a00af50bbb0f4f6b962fad5daca43e63
aymane <minescopola@gmail.com>
2023-08-08 22:53:01 +0000

    fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -21,12 +21,16 @@ char *str_concat(char *s1, char *s2)
 	{
 		return (NULL);
 	}
-
-	for (len1 = 0; s1[len1]; len1++)
-		;
-	for (len2 = 0; s2[len2]; len2++)
-		;
-
+	if (s1)
+	{
+		for (len1 = 0; s1[len1]; len1++)
+			;
+	}
+	if (s2)
+	{
+		for (len2 = 0; s2[len2]; len2++)
+			;
+	}
 
 	catstr = malloc((sizeof(char) * len1) + (sizeof(char) * len2) + 1);
 
