b669f16061aeb6f04a4944dde8bdbf541fccef32
aymane <minescopola@gmail.com>
2023-08-09 00:37:13 +0000

    est

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -21,16 +21,22 @@ char *str_concat(char *s1, char *s2)
 	{
 		return (NULL);
 	}
-	if (s1 != NULL)
+	if (s1 = NULL)
 	{
-		for (len1 = 0; s1[len1]; len1++)
-			;
+		s1 = '';
 	}
-	if (s2 != NULL)
+	if (s2 = NULL)
 	{
-		for (len2 = 0; s2[len2]; len2++)
-			;
+		s2 = '';
 	}
+	for (len1 = 0; s1[len1]; len1++)
+
+			;
+
+	for (len2 = 0; s2[len2]; len2++)
+
+			;
+
 
 	catstr = malloc((sizeof(char) * len1) + (sizeof(char) * len2) + 1);
 
