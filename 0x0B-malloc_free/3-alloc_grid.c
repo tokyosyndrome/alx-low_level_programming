cb6609b83f9962a2a1dbea8561f3529859cd4c1c
aymane <minescopola@gmail.com>
2023-08-09 01:24:57 +0000

    grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* alloc_grid - concatenates string
+* @width: width of atric
+* @height: height of matrix
+* Return: NULL or ptr to grid location
+*/
+
+char **alloc_grid(int width, int height)
+{
+	int **grid;
+
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	grid = malloc(sizeof(int *) * height);
+	if (!grid)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(grid[j]);
+			}
+			free(grid);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+		
+	}
+	return (grid);
+}
