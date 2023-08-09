ec9cc2a91e420e24ce6397b38cd927d518dbc19f
aymane <minescopola@gmail.com>
2023-08-09 00:43:42 +0000

    betty

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /**
-* _strconcat - duplicates string
+* _str_concat - duplicates string
 * @s1: str1
 * @s2: str2
 * Return: NULL or ptr to str location
@@ -26,31 +26,22 @@ char *str_concat(char *s1, char *s2)
 		s2 = "";
 	}
 	for (len1 = 0; s1[len1]; len1++)
-
-			;
-
+		;
 	for (len2 = 0; s2[len2]; len2++)
-
-			;
-
-
+		;
 	catstr = malloc((sizeof(char) * len1) + (sizeof(char) * len2) + 1);
-
 	if (!catstr)
 	{
 		return (NULL);
 	}
-
 	for (i = 0; i < len1; i++)
 	{
 		catstr[i] = s1[i];
 	}
-
 	for (j = 0; j < len2; j++, i++)
 	{
 		catstr[i] = s2[j];
 	}
 	catstr[i] = '\0';
-
 	return (catstr);
 }
