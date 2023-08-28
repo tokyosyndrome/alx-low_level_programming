1ccd645e4010c501f7cc8848b48eeddd9b37d4ae
aymane <minescopola@gmail.com>
2023-08-28 16:34:25 +0000

    fib four

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ *main - print number total
+ *Return: the sum of numbers
+ */
+
+int main(void)
+{
+	long a, b, i, res, start;
+
+	a = 0;
+	b = 1;
+	start = 1;
+	for (i = 0; i < 98; i++)
+	{
+		res = a + b;
+		a = b;
+		b = res;
+		if (start == 0)
+			printf(", ");
+		printf("%lu", res);
+		start = 0;
+	}
+	printf("\n");
+	return (0);
+}
