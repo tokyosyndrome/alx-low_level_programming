b0197237f5671069f008c2cfb7bac1e119c90508
aymane <minescopola@gmail.com>
2023-08-30 14:20:25 +0000

    !

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,9 +8,9 @@
 
 int _strlen_recursion(char *s)
 {
-	if (!(*s))
+	if (*s == '\0')
 	{
-		return;
+		return (0);
 	}
-	return(1 + _strlen_recursion((s + 1));
+	return(_strlen_recursion((s + 1) + 1);
 }
